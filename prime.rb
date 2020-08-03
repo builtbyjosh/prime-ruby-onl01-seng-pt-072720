0a05daba3577deddf6980104c77523e6f57a4a7e
Joshua Collins <pc.refuge@gmail.com>
2020-08-03 04:46:22 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,8 @@
 # Add  code here!
 def prime?(num)
   (2..(num - 1)).each do |i|
-    return false if (num % i == 0 || num < 2)
+    return false if num % i == 0
   end
+  return false if num < 2
   true
 end
